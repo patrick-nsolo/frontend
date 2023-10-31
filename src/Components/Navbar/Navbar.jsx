801be00a94c4dfeea2767b86b08a3434c7a47a9a
Patrick Nsolo <64790572+patrick-nsolo@users.noreply.github.com>
2023-10-31 20:31:26 +0100

    update shop navigation login

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -16,7 +16,10 @@ const Navbar = () => {
             <li>Women</li>
             <li>Kids</li>
         </ul>
-      
+        <div className='nav-login-cart'>
+            <button>Login</button>
+            <img src={cart_icon} alt=''/>
+        </div>
     </div>
   )
 }
