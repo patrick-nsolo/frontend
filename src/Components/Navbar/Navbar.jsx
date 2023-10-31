61c867123cd95c3e279aaa1a72f5ff2b831e8410
Patrick Nsolo <64790572+patrick-nsolo@users.noreply.github.com>
2023-10-31 20:12:22 +0100

    logo update

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -6,6 +6,9 @@ import cart_icon from '../Assets/cart_icon.png';
 const Navbar = () => {
   return (
     <div className='navbar'>
+        <div className='nav-logo'>
+             
+        </div>
       
     </div>
   )
