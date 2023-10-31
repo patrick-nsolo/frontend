dcb09693d19f5ad22a0306b21131dbae536eb0dd
Patrick Nsolo <64790572+patrick-nsolo@users.noreply.github.com>
2023-10-31 21:26:43 +0100

    update navigation click and change

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ import cart_icon from '../Assets/cart_icon.png';
 
 const Navbar = () => {
     //create variable to facilitate navigation link change
-    
+    const [menu,setMenu] = useState('shop');
   return (
     <div className='navbar'>
         <div className='nav-logo'>
@@ -13,10 +13,10 @@ const Navbar = () => {
             <p>MAYOR STORE</p>     
         </div>
         <ul className='nav-menu'>
-            <li>Shop <hr/></li>
-            <li>Men</li>
-            <li>Women</li>
-            <li>Kids</li>
+            <li onClick{() => {setMenu()}}>Shop <hr/></li>
+            <li onClick{() => {setMenu()}}>Men</li>
+            <li onClick{() => {setMenu()}}>Women</li>
+            <li onClick{() => {setMenu()}}>Kids</li>
         </ul>
         <div className='nav-login-cart'>
             <button>Login</button>
