0fb8c5a557bae6dd4f78e98ef364a15eacb26dbf
Patrick Nsolo <64790572+patrick-nsolo@users.noreply.github.com>
2023-11-01 17:41:51 +0100

    update all pages

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import Shop from './Pages/Shop';
 import ShopCategory from './Pages/ShopCategory';
 import Product from './Pages/Product';
+import Cart from './Pages/Cart';
+import LoginSignup from './Pages/LoginSignup'
 
 function App() {
   return (
@@ -16,8 +18,10 @@ function App() {
         <Route path='/women' element={<ShopCategory category='women'/>}/>
         <Route path='/kids' element={<ShopCategory category='kids'/>}/>
         <Route path='/product' element={<Product/>}>
-
+          <Route path=':productId' element={<Product/>}/>
         </Route>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/login' element={<LoginSignup/>}/>
       </Routes>
 
       </BrowserRouter>  
