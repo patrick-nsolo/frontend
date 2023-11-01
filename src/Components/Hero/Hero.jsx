cd673cbabc8024436858518868fbfc51a618fe74
Patrick Nsolo <64790572+patrick-nsolo@users.noreply.github.com>
2023-11-01 18:57:54 +0100

    update hero  elements
    update hero elements

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import './Hero.css';
+import hand_icon from '../Assets/hand_icon.png';
 
 const Hero = () => {
   return (
     <div className='hero'>
         <div className='hero-left'>
+            <h2>NEW ARRIVALS ONLY</h2>
+            <div>
+                <div className='hero-hand-icon'>
+                    <img src={hand_icon} alt=''/>
 
+                </div>
+                <p>Collections</p>
+                <p>For Everyone</p>
+            </div>
+            <div className='hero-latest-btn'></div>
         </div>
         <div className='hero-right'>
 
