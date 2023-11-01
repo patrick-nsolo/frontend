893b5acdc48c0c57cde2aa81959efb0132093a6d
Patrick Nsolo <64790572+patrick-nsolo@users.noreply.github.com>
2023-11-01 16:11:14 +0100

    Pages creation

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Shop = () => {
+const LoginSignup = () => {
   return (
     <div>
       
@@ -8,4 +8,4 @@ const Shop = () => {
   )
 }
 
-export default Shop;
+export default LoginSignup;
