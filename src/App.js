3c059d012b95df313a8da167d127e171da495201
Patrick Nsolo <64790572+patrick-nsolo@users.noreply.github.com>
2023-11-01 17:10:46 +0100

    Update Browser Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Navbar from './Components/Navbar/Navbar';
 import './App.css';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
 
 function App() {
   return (
     <div >
+      <BrowserRouter>
       <Navbar/>
-      
+
+      </BrowserRouter>  
     </div>
   );
 }
